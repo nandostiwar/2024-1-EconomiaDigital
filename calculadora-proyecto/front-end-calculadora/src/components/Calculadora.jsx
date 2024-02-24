5f5029aa8f270f4aa00d62361aa16c79961c8fff
dennisfrancosma <124455177+dennisfrancosma@users.noreply.github.com>
2024-02-24 18:32:53 -0500

    add new apis

--- calculadora-proyecto/front-end-calculadora/src/components/Calculadora.jsx
+++ calculadora-proyecto/front-end-calculadora/src/components/Calculadora.jsx
@@ -32,6 +32,9 @@ function Calculadora(){
                 <input type="submit" className="btnEnviar" value="sumar" onClick={handleSubmit}/>
                 <input type="submit" className="btnEnviar" value="restar" onClick={handleSubmit}/>
                 <input type="submit" className="btnEnviar" value="multiplicar" onClick={handleSubmit}/>
+                <input type="submit" className="btnEnviar" value="dividir" onClick={handleSubmit}/>
+                <input type="submit" className="btnEnviar" value="raizuno" onClick={handleSubmit}/>
+                <input type="submit" className="btnEnviar" value="raizdos" onClick={handleSubmit}/>
             </form>
             <Resultado resultado={"El resultado es "+ resultado}/>
         </div>
