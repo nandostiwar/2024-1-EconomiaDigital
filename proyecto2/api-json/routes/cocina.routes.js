4ab973047ee6f30557091891b85f0e6df01f4d9c
Danny3018 <dannymed.3004@gmail.com>
2024-06-01 13:33:32 -0500

    Elimina

--- proyecto2/api-json/routes/cocina.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const pedidosController = require('../routes/controllers/pedidosControllers');
-const cocinaController = require('../routes/controllers/cocinaControllers');
-
-router.get('/', pedidosController.getAllPedidos);
-router.put('/:id', cocinaController.modificarEstadoPedido);
-
-module.exports = router;
--- proyecto2/api-json/routes/ventas.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ventasController = require('../routes/controllers/ventasController');
-
-// Ruta para obtener todas las ventas
-router.get('/', ventasController.getAllVentas);
-
-module.exports = router;
